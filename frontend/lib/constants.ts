425a127b0fc68a54c004d214bfe365c2a0e433af
eric-bach <bach.eric@email.com>
2025-09-29 11:05:54 -0600

    Update ADO field names and fix error when 0 tasks exist on a work item

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,4 +1,4 @@
-// Azure DevOps dropdown options
+// TODO: Update the Azure DevOps dropdown options
 export const AREA_PATHS = ['eric-test'];
 export const BUSINESS_UNITS = ['Membership', 'Registries', 'Rewards'];
 export const SYSTEMS = ['ARAR', 'Gift Card System', 'iMIS', 'L2', 'VRAR'];
