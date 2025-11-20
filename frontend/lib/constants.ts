5e73a2d5d6dc3cd9a3a84f6a499ea9f612d25614
Eric Bach <43318858+eric-bach@users.noreply.github.com>
2025-11-20 11:35:13 -0700

    Add blue team to pilot (#32)
    Co-authored-by: eric-bach <bach.eric@email.com>

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,4 +1,8 @@
 // TODO: Update the Azure DevOps dropdown options
-export const AREA_PATHS = ['eric-test', 'AMA-Ent\\Software Development\\Orange Team'];
-export const BUSINESS_UNITS = ['Membership', 'Registries'];
-export const SYSTEMS = ['iMIS', 'Omni-Admin – Service Queuing'];
+export const AREA_PATHS = [
+  'eric-test',
+  'AMA-Ent\\Software Development\\Orange Team',
+  'AMA-Ent\\Software Development\\Blue Team',
+];
+export const BUSINESS_UNITS = ['IT', 'Membership', 'Registries', 'Travel'];
+export const SYSTEMS = ['iMIS', 'Omni-Admin – Service Queuing', 'Omni-POS'];
